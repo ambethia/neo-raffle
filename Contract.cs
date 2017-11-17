9510cf75418ea184159414d5cc6c5e3455f3a1fb
Jason L Perry <jasper@ambethia.com>
2017-11-16 20:20:47 -0500

    We have a raffle.

--- Contract.cs
+++ Contract.cs
@@ -7,9 +7,11 @@ namespace Raffle
 {
     public class Contract : SmartContract
     {
-        private const int minAward = 1 * 100000000;
+        public static readonly byte[] Owner = { 130, 8, 5, 240, 104, 143, 118, 63, 250, 10, 101, 186, 42, 163, 225, 40, 156, 230, 65, 138 };
+        public const long MaxRake = 1;
 
-        // private static readonly byte[] gas_asset_id = { 231, 45, 40, 105, 121, 238, 108, 177, 183, 230, 93, 253, 223, 178, 227, 132, 16, 11, 141, 20, 142, 119, 88, 222, 66, 228, 22, 139, 113, 121, 44, 96 };
+        private static readonly byte[] gas_asset_id = { 231, 45, 40, 105, 121, 238, 108, 177, 183, 230, 93, 253, 223, 178, 227, 132, 16, 11, 141, 20, 142, 119, 88, 222, 66, 228, 22, 139, 113, 121, 44, 96 };
+        private const long factor = 100000000;
 
         public static object Main(string operation, byte[] txid)
         {
@@ -18,96 +20,35 @@ namespace Raffle
                 Transaction tx = (Transaction)ExecutionEngine.ScriptContainer;
                 TransactionOutput[] outputs = tx.GetOutputs();
                 byte[] receiver = ExecutionEngine.ExecutingScriptHash;
-                if (outputs.Length == 1)
+
+                if (outputs.Length == 1 && outputs[0].ScriptHash == receiver)
                 {
-                    // If we're sending funds to ourself, then this is a sweep tx,
-                    // otherwise it must be a payout.
-                    TransactionOutput output = outputs[0];
-                    if (output.ScriptHash == receiver)
-                    {
-                        return Sweep(tx);
-                    }
-                    else
-                    {
-                        return Payout(tx, output);
-                    }
+                    return Runtime.CheckWitness(Owner);
                 }
                 else
                 {
-                    return false;
+                    return Payout(tx, outputs);
                 }
-            }
 
+            }
             if (Runtime.Trigger == TriggerType.Application)
             {
-                if (operation == "last")
-                {
-                    return LastPayoutTime();
-                }
-
-                if (operation == "sweep")
-                {
-                    Transaction tx = Blockchain.GetTransaction(txid);
-                    return Sweep(tx);
-                }
-
                 if (operation == "drawing")
                 {
-                    return FindWinner(txid);
-                }
-
-                if (operation == "winner")
-                {
-                    byte[] winner = Storage.Get(Storage.CurrentContext, "winner");
-                    return winner;
+                    return Drawing(txid);
                 }
             }
-
             return false;
         }
 
-        // The sweep transaction's purpose is to take all of the deposits into the contract and bundle them into one TX,
-        // and TXID makes it easier to enumerate all the transactions in the FindWinner function later.
-        private static bool Sweep(Transaction tx)
-        {
-            Runtime.Log("Sweep");
-            Header header = Blockchain.GetHeader(Blockchain.GetHeight());
-            uint elapsed = header.Timestamp - LastPayoutTime();
-            uint minDelta = 60;
-            if (elapsed < minDelta)
-            {
-                Runtime.Log("Wut");
-                return false;
-            }
-
-            byte[] gas_asset_id = { 231, 45, 40, 105, 121, 238, 108, 177, 183, 230, 93, 253, 223, 178, 227, 132, 16, 11, 141, 20, 142, 119, 88, 222, 66, 228, 22, 139, 113, 121, 44, 96 };
-            Runtime.Notify("gas_asset_id:", gas_asset_id);
-            TransactionOutput[] outputs = tx.GetReferences();
-
-            // Sum up the amount in this TX
-            long awardTotal = 0;
-            foreach (TransactionOutput output in outputs)
-            {
-                if (output.AssetId == gas_asset_id)
-                {
-                    awardTotal += output.Value;
-                }
-            }
-
-            // Verify the minimum payout is met.
-            if (awardTotal < minAward) return false;
-
-            return true;
-        }
-
         // Given a TXID, loop through them and pick a winner.
-        private static bool FindWinner(byte[] txid)
+        private static bool Drawing(byte[] txid)
         {
-            Runtime.Log("Find Winner");
+            if (!Runtime.CheckWitness(Owner)) return false;
+
             Header header = Blockchain.GetHeader(Blockchain.GetHeight());
             Transaction tx = Blockchain.GetTransaction(txid);
             TransactionOutput[] outputs = tx.GetReferences();
-            byte[] gas_asset_id = { 231, 45, 40, 105, 121, 238, 108, 177, 183, 230, 93, 253, 223, 178, 227, 132, 16, 11, 141, 20, 142, 119, 88, 222, 66, 228, 22, 139, 113, 121, 44, 96 };
 
             // Removing this line breaks everything, even through we dont use it anywhere. Go figure.
             byte[] receiver = ExecutionEngine.ExecutingScriptHash;
@@ -127,8 +68,9 @@ namespace Raffle
             long randomNumber = (long)(header.ConsensusData >> 32);
             // Math tricks to pick a number between 0 and awardTotal
             long winningTicket = (awardTotal * randomNumber) >> 32;
-
+            // We're going to sum up the tickets in the bucket to find the winner.
             long bucket = 0;
+
             foreach (var input in tx.GetInputs())
             {
                 // Some more trickery to find the senders address.
@@ -145,6 +87,7 @@ namespace Raffle
                     // Add the amount sent into the bucket, once we reach the magic number,
                     // we have a winner, this is the ticket/transaction that includes the winning number.
                     bucket += ticketValue;
+
                     if (bucket >= winningTicket)
                     {
                         // Store these details so we can use them to generate the payout later.
@@ -152,35 +95,38 @@ namespace Raffle
                         Storage.Put(Storage.CurrentContext, "payout", Blockchain.GetHeight());
                         Storage.Put(Storage.CurrentContext, "sweep", tx.Hash);
                         Storage.Put(Storage.CurrentContext, "amount", awardTotal);
+
                         return true;
                     }
                 }
             }
-            return true;
-        }
 
-        // Verify the payout transaction is only spending the sweep tx from earlier and is being paid to the winner.
-        private static bool Payout(Transaction tx, TransactionOutput output)
-        {
-            Runtime.Log("Payout");
-            byte[] winner = Storage.Get(Storage.CurrentContext, "winner");
-            byte[] sweep = Storage.Get(Storage.CurrentContext, "sweep");
-            BigInteger amount = Storage.Get(Storage.CurrentContext, "amount").AsBigInteger();
-            TransactionInput[] inputs = tx.GetInputs();
-            // TODO: add a rake
-            if (inputs.Length != 1) return false;
-            if (inputs[0].PrevHash == sweep && output.ScriptHash == winner && output.Value >= amount) // gte here so we can send invoke fee amounts that sprinkle in.
-            {
-                return true;
-            }
             return false;
         }
 
-        private static uint LastPayoutTime()
+        // Verify the payout transaction is only spending the sweep tx from earlier and is being paid to the winner.
+        // NOTE: Storage.Get appears to still fail here, making in-contract verification of the winner not possible.
+        // However, the winner for each round is still persisted in the blockchain for 3rd-party verification.
+        // In theory, uncommenting these lines in the future will allow the contract to verify payouts.
+        private static bool Payout(Transaction tx, TransactionOutput[] outputs)
         {
-            BigInteger lastPayout = Storage.Get(Storage.CurrentContext, "payout").AsBigInteger();
-            Header header = Blockchain.GetHeader((uint)lastPayout);
-            return header.Timestamp;
+            // byte[] winner = Storage.Get(Storage.CurrentContext, "winner");
+            // byte[] sweep = Storage.Get(Storage.CurrentContext, "sweep");
+            // TransactionOutput payout = outputs[0];
+            TransactionOutput rake = outputs[1];
+            TransactionInput[] inputs = tx.GetInputs();
+
+            // if (inputs.Length != 1) return false;
+
+            // if (inputs[0].PrevHash != sweep) return false;
+
+            // if (payout.ScriptHash != winner) return false;
+
+            if (rake.ScriptHash != Owner) return false;
+
+            if (rake.Value > MaxRake * factor) return false;
+
+            return Runtime.CheckWitness(Owner);
         }
     }
 }
